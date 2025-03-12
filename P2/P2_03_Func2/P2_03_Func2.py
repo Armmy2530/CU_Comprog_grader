6c1032aa436800365631c9c21ceacf675e9c6c0a
Armmy2530 <sutigran2557@gmail.com>
2025-03-12 14:18:22 +0700

    update

--- P2/P2_03_Func2/P2_03_Func2.py
+++ P2/P2_03_Func2/P2_03_Func2.py
@@ -1,37 +1,38 @@
 def convex_polygon_area(p):
-    return 0.5*(sum([p[i][0]*p[i+1][1] for i in range(len(p)-1)])-sum([p[i][1]*p[i+1][0] for i in range(len(p)-1)]))
-    pass
+    a = [p[i][0] * p[i+1 if i < len(p)-1 else 0][1] for i in range(len(p))]
+    b = [p[i][1] * p[i+1 if i < len(p)-1 else 0][0] for i in range(len(p))]
+    return abs(0.5*(sum(a)-sum(b)))
 def is_heterogram(s):
     data = {}
-    for i in s:
-        if (i != " "):
-            if(i in data.keys()):
-                data[i] += 1
-            else:
-                data[i] = 1
-            if(data[i] == 2):
-                return False
+    for j in s.split():
+        j = j.lower()
+        for i in j:
+            if (i in "abcdefghijklmnopqrstuvwxyz"):
+                if(i in data.keys()):
+                    data[i] += 1
+                else:
+                    data[i] = 1
+                if(data[i] == 2):
+                    return False
     return True
 def replace_ignorecase(s, a, b):
-    pass
+    ans = ""
+    index = 0
+    a = a.lower()
+    while True:
+        s_lower = s.lower()
+        index_found = s_lower[index:].find(a)
+        if(index_found != -1):
+            ans += s[index:index_found+index] + b
+        else:
+            ans += s[index:]
+            break
+        index += index_found + len(a)
+    return ans
+
 def top3(votes):
-    pass
+    rank = sorted(votes.items(),key = lambda x:(-x[1],x[0]))
+    return [i[0] for i in rank[:3]]
 
-# for k in range(2):
-#     exec(input().strip())
-
-print(convex_polygon_area([[0,0], [0,3], [4,0]]))
-print(convex_polygon_area([[0,0], [4,0], [0,3]]))
-# 6.0
-# 6.0
-print(is_heterogram("The big dwarf only jumps."))
-print(is_heterogram("Java"))
-# True
-# False
-# print(replace_ignorecase("Python is hard", "Hard", "easy"))
-# print(replace_ignorecase("AAabaAA", "Aa", "Aaa"))
-# Python is easy
-# AaaabAaaA
-# v = {"A": 8888, "B": 6666, "C": 7777, "X":6666}
-# print(top3(v))
-# ['A', 'C', 'B']
+for k in range(2):
+  exec(input().strip())
