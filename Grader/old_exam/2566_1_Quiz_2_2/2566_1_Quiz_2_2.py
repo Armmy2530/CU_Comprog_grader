d959e0ece89b2c8d62829696b41b0c666d297ff5
Armmy2530 <Sutigran2557@gmail.com>
2025-03-19 22:39:08 +0700

    update

--- Grader/old_exam/2566_1_Quiz_2_2/2566_1_Quiz_2_2.py
+++ Grader/old_exam/2566_1_Quiz_2_2/2566_1_Quiz_2_2.py
@@ -1,5 +1,4 @@
-filename = "2110100.txt"
-# filename = input().strip()
+filename = input().strip()
 aj_id = input().strip()
 
 file = open(filename)
@@ -16,3 +15,12 @@ for i in student:
     if i[4] == aj_id:
         if(i[3] not in sections):
             sections.append(i[3])
+        if i[1] == 'M':
+            male += 1
+        else:
+            female += 1
+            
+if len(sections) != 0:
+    print(f"Section{'s' if len(sections) > 1 else ''}: {",".join(sections)} --> F = {female}, M = {male} ")
+else:
+    print("Not found")
